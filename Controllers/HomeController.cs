90d79c70ca8aede4cce579f996ba6f808b70ce9c
Natalia804 <nlys@student.agh.edu.pl>
2023-05-23 19:55:42 +0200

    Home Controllers dokumentacja

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,46 +14,87 @@ namespace AkwadratDesign.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Metoda obsługuje żądanie GET na adres /Home/Index.
+        /// </summary>
+        /// <returns> Zwraca widok domyślny dla strony głównej.</returns>
         public IActionResult Index()
         {
             return View();
         }
+
+        /// <summary>
+        /// Metoda obsługuje żądanie GET na adres /agnieszka-osypanka/.
+        /// </summary>
+        /// <returns>Zwraca widok z informacjami o Agnieszce Osypance-architektce.</returns>
         [Route("/agnieszka-osypanka/")]
         public IActionResult About()
         {
             return View();
         }
+
+        /// <summary>
+        /// Metoda obsługuje żądanie GET na adres /kontakt/.
+        /// </summary>
+        /// <returns> Zwraca widok z formularzem kontaktowym.</returns>
         [Route("/kontakt/")]
         public IActionResult Contact()
         {
             return View();
         }
+
+        /// <summary>
+        /// Metoda obsługuje żądanie GET na adres /wspolpraca/.
+        /// </summary>
+        /// <returns> Zwraca widok z informacjami o współpracy.</returns>
         [Route("/wspolpraca/")]
         public IActionResult Collaborations()
         {
             return View();
         }
+
+        /// <summary>
+        /// Metoda obsługuje żądanie GET na adres /realizacje/.
+        /// </summary>
+        /// <returns> Zwraca widok z portfolio realizacji.</returns>
         [Route("/realizacje/")]
         public IActionResult Portfolio()
         {
             return View();
         }
+
+        /// <summary>
+        /// Metoda obsługuje żądanie GET na adres /admin.
+        /// </summary>
+        /// <returns> Zwraca widok panelu administracyjnego.</returns>
         [Route("/admin")]
         public IActionResult Admin()
         {
             return View();
         }
+        /// <summary>
+        /// Metoda obsługuje żądanie GET na adres /admin-panel.
+        /// </summary>
+        /// <returns> Wymaga wcześniejszego uwierzytelnienia użytkownika. Zwraca widok panelu administracyjnego.</returns>
         [Authorize]
         [Route("/admin-panel")]
         public IActionResult AdminPanel()
         {
             return View();
         }
+        /// <summary>
+        /// Metoda obsługuje żądanie GET na adres /polityka-prywatnosci/.
+        /// </summary>
+        /// <returns>  Zwraca widok z polityką prywatności.</returns>
         [Route("/polityka-prywatnosci/")]
         public IActionResult Privacy()
         {
             return View();
         }
+        /// <summary>
+        /// Metoda obsługuje żądanie GET na adres /Home/Error.
+        /// </summary>
+        /// <returns>  Zwraca widok z informacją o błędzie. Przekazuje identyfikator błędu do widoku.</returns>
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
