7a705a7e91be0b50a172275efed81c2abd758e4c
alekfolfas <alekfolfas@student.agh.edu.pl>
2023-05-24 11:06:36 +0200

    Exception hadling ad http status code

--- Models/DbModels/ProjectFirm.cs
+++ Models/DbModels/ProjectFirm.cs
@@ -12,7 +12,7 @@
     {
         public int FirmsId { get; set; }
         public int ProjectsId { get; set; }
-        public Firm Firm { get; set; } = null!;
-        public Project Project { get; set; } = null!;
+        public Firm? Firm { get; set; } = null!;
+        public Project? Project { get; set; } = null!;
     }
 }
